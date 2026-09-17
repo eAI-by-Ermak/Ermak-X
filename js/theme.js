04393fc8b2042fbfd303027f918c14888e53f7df
Ермак <ermakartekovec@gmail.com>
2026-09-17 16:52:57 +0600

    Cleanup: remove stub js/theme.js

--- js/theme.js
+++
@@ -1,8 +0,0 @@
-/* Ermak X — Liquid Glass pointer lighting */
-/* Re-export / load the shared theme.js during migration */
-(function(){
-  var s=document.createElement("script");
-  s.src="../theme.js";
-  s.defer=true;
-  document.head.appendChild(s);
-})();
